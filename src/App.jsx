9032b3cabb43844b1a52fb897943d944a8609e26
Fay <z5421674@ad.unsw.edu.au>
2026-04-14 17:58:53 +1000

    changed router to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SinglesPage from './SinglesPage';
@@ -14,7 +14,7 @@ function App () {
 
   return (
     <>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Dashboard/>} />
           <Route path="/singles" element={<SinglesPage/>} />
@@ -23,7 +23,7 @@ function App () {
           <Route path="/about" element={<AboutPage/>} />
           <Route path="/contact" element={<ContactPage/>} />
           </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
